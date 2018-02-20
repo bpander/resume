3c94bfbbf7d23d87b04a5feb0ab24ad1041caaba
Brad Anderson <banderso@nerdery.com>
2018-02-19 21:29:35 -0600

    Minor formatting tweak and removing unnecessary skill. Fixes build issue.

--- tools/tasks/buildStyles.js
+++ tools/tasks/buildStyles.js
@@ -4,15 +4,6 @@
 module.exports = function(grunt) {
     var shouldMinify = !grunt.option('dev');
 
-    // Help Grunt find the right plugins at runtime
-    require('jit-grunt')(grunt, {
-        useminPrepare: 'grunt-usemin'
-    });
-
-    // Clear out any previously generated usemin task configuration
-    grunt.config.set('concat', undefined);
-    grunt.config.set('cssmin', undefined);
-
     grunt.config.merge({
         // Copies static files for non-optimized builds
         copy: {
@@ -24,24 +15,6 @@ module.exports = function(grunt) {
                     src: ['assets/{styles,vendor}/**/*.css']
                 }]
             }
-        },
-
-        // Searches for build comment blocks (`<!-- build:css -->`) and generates
-        // the appropriate `concat` and `cssmin` configuration.
-        useminPrepare: {
-            options: {
-                root: '<%= env.DIR_SRC %>',
-                staging: '<%= env.DIR_TMP %>',
-                dest: '<%= env.DIR_DEST %>',
-                flow: {
-                    buildStyles: {
-                        // Force css only
-                        steps: { css: ['concat', 'cssmin'], js: [] },
-                        post: {}
-                    }
-                }
-            },
-            buildStyles: ['<%= env.DIR_SRC %>/**/*.html']
         },
 
         sass: {
@@ -68,19 +41,13 @@ module.exports = function(grunt) {
 
             grunt.config.set(name, JSON.parse(config));
         }
-
-        scrub('concat');
-        scrub('cssmin');
     });
 
     grunt.registerTask('buildStyles',
         shouldMinify
             ? [
                 'sass:buildStyles',
-                'useminPrepare:buildStyles',
                 'scrub:buildStyles',
-                'concat:generated',
-                'cssmin:generated'
             ]
             : [
                 'copy:buildStyles',
